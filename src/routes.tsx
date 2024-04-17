169d2f2979828e57187e0cff7894f2bdd24c017c
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2024-04-17 00:47:05 -0300

    feat: Adicionando layouts para app e auth

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Dashboard } from "./pages/app/dashboard";
 import { Signin } from "./pages/auth/signin";
+import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
 
 export const router = createBrowserRouter([
-    { path: "/", element: <Dashboard /> },
-    { path: "/auth/sign-in", element: <Signin /> }
+
+    {
+        path: '/',
+        element: <AppLayout/>,
+        children:  [{ path: "/", element: <Dashboard /> }]
+    },
+    {
+        path: '/',
+        element: <AuthLayout/>,
+        children:  [{ path: "/auth/sign-in", element: <Signin /> }]
+    }
+   
 ]);
 
